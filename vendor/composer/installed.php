0406da05b4ac4271ec6dce32ff5987f3908c786c
Ashu Raj Srivastav <161217755+AshurajSrivastav01@users.noreply.github.com>
2026-07-14 06:50:43 +0530

    just added a new controller to check the setup is working or not

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ashuraj/student-api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '49b0284300d28093f5abf3579762b93cea847fb2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ashuraj/student-api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '49b0284300d28093f5abf3579762b93cea847fb2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
