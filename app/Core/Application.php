bd410e6b038d2019081690908bb99c083df3ed04
Ashu Raj Srivastav <161217755+AshurajSrivastav01@users.noreply.github.com>
2026-07-17 07:14:45 +0530

    we have completed a version 1 of the middleware

--- app/Core/Application.php
+++ app/Core/Application.php
@@ -6,6 +6,7 @@ class Application
 {
     public function run(): void
     {
+        // Here we are just calling the 'HttpKernel' class and its 'handle' method to process the incoming HTTP request.
         $kernel = new HttpKernel();
         $kernel->handle();
     }
